c1ba271421756e28574732fdbc7333d6cb8d461d
Gilles Zunino <GillesZunino@users.noreply.github.com>
2016-12-31 14:02:38 -0800

    Empty project structure

---
+++ src/Echarde.Net46/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿// -----------------------------------------------------------------------------------
+// Copyright 2016, Gilles Zunino
+// -----------------------------------------------------------------------------------
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Echarde")]
+[assembly: AssemblyDescription("Azure table storage secondary indices library")]
+[assembly: AssemblyConfiguration(".NET Framework")]
+[assembly: AssemblyCompany("Akakani")]
+[assembly: AssemblyProduct("Echarde")]
+[assembly: AssemblyCopyright("Copyright © Gilles Zunino 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
