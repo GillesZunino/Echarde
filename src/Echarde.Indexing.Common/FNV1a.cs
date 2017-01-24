5fcbac84f5f2cc718151acacbc35d7e1664ce819
Gilles Zunino <GillesZunino@users.noreply.github.com>
2017-01-23 22:49:17 -0800

    Cleanup FNV1a Xml documentation comments

--- src/Echarde.Indexing.Common/FNV1a.cs
+++ src/Echarde.Indexing.Common/FNV1a.cs
@@ -12,12 +12,12 @@ namespace Echarde.Indexing
     public static class FNV1a
     {
         /// <summary>
-        /// Default FNV1A 32 seed.
+        /// Default FNV1a 32 seed.
         /// </summary>
         public const uint FNV1_32A_INIT = 0x811c9dc5;
 
         /// <summary>
-        /// Default FNV1A 64 seed.
+        /// Default FNV1a 64 seed.
         /// </summary>
         public const ulong FNV1A_64_INIT = 0xcbf29ce484222325UL;
 
@@ -25,6 +25,7 @@ namespace Echarde.Indexing
         /// Computes the 32-bit FNV1a hash.
         /// </summary>
         /// <param name="str">Input string.</param>
+        /// <returns>32 bits FNV1a hash of <paramref name="str"/>.</returns>
         public static uint Fnv1A32(string str)
         {
             return Fnv1A32(Encoding.UTF8.GetBytes(str), FNV1_32A_INIT);
@@ -34,6 +35,7 @@ namespace Echarde.Indexing
         /// Computes the 32-bit FNV1a hash.
         /// </summary>
         /// <param name="data">Input data.</param>
+        /// <returns>32 bits FNV1a hash of <paramref name="data"/>.</returns>
         public static uint Fnv1A32(byte[] data)
         {
             return Fnv1A32(data, FNV1_32A_INIT);
@@ -43,6 +45,7 @@ namespace Echarde.Indexing
         /// Computes the 64-bit FNV1a hash.
         /// </summary>
         /// <param name="str">Input string.</param>
+        /// <returns>64 bits FNV1a hash of <paramref name="str"/>.</returns>
         public static ulong Fnv1A64(string str)
         {
             return Fnv1A64(Encoding.UTF8.GetBytes(str), FNV1A_64_INIT);
@@ -52,16 +55,18 @@ namespace Echarde.Indexing
         /// Computes the 64-bit FNV1a hash.
         /// </summary>
         /// <param name="data">Input data.</param>
+        /// <returns>64 bits FNV1a hash of <paramref name="data"/>.</returns>
         public static ulong Fnv1A64(byte[] data)
         {
             return Fnv1A64(data, FNV1A_64_INIT);
         }
 
         /// <summary>
-        /// Computes thet 64-bit FNV1a hash.
+        /// Computes thet 32-bit FNV1a hash.
         /// </summary>
         /// <param name="data">Input data.</param>
-        /// <param name="seed">Seed</param>
+        /// <param name="seed">Seed.</param>
+        /// <returns>32 bits FNV1a hash of <paramref name="data"/>.</returns>
         public static uint Fnv1A32(byte[] data, uint seed)
         {
             unchecked
@@ -81,6 +86,7 @@ namespace Echarde.Indexing
         /// </summary>
         /// <param name="data">Input data.</param>
         /// <param name="seed">Seed.</param>
+        /// <returns>64 bits FNV1a hash of <paramref name="data"/>.</returns>
         public static ulong Fnv1A64(byte[] data, ulong seed)
         {
             unchecked
