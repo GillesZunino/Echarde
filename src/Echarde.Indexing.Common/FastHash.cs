422bd864bbda7a3540977a2a82cf5c0318262acb
Gilles Zunino <GillesZunino@users.noreply.github.com>
2017-01-29 20:25:13 -0800

    FastHash hash implementation and tests

---
+++ src/Echarde.Indexing.Common/FastHash.cs
@@ -0,0 +1,144 @@
+﻿// -----------------------------------------------------------------------------------
+// Copyright 2017, Gilles Zunino
+// -----------------------------------------------------------------------------------
+
+using System.Text;
+
+namespace Echarde.Indexing
+{
+    /// <summary>
+    /// Calculates FastHash - See https://github.com/rurban/smhasher
+    /// </summary>
+    public static class FastHash
+    {
+        /// <summary>
+        /// Default FastHash 32 seed.
+        /// </summary>
+        public const uint DefaultSeed32 = 0U;
+
+        /// <summary>
+        /// Default FastHash 64 seed.
+        /// </summary>
+        public const ulong DefaultSeed64 = 0UL;
+
+
+        /// <summary>
+        /// Computes the 32 FastHash. 
+        /// </summary>
+        /// <param name="str">Input string.</param>
+        /// <returns>32 bits Fasthash of <paramref name="str"/>.</returns>
+        public static uint FastHash32(string str)
+        {
+            return FastHash32(Encoding.UTF8.GetBytes(str));
+        }
+
+        /// <summary>
+        /// Computes the 32 FastHash. 
+        /// </summary>
+        /// <param name="data">Input data.</param>
+        /// <returns>32 bits Fasthash of <paramref name="data"/>.</returns>
+        public static uint FastHash32(byte[] data)
+        {
+            return FastHash32(data, DefaultSeed32);
+        }
+
+        /// <summary>
+        /// Computes the 64 FastHash. 
+        /// </summary>
+        /// <param name="str">Input string.</param>
+        /// <returns>64 bits Fasthash of <paramref name="str"/>.</returns>
+        public static ulong FastHash64(string str)
+        {
+            return FastHash64(Encoding.UTF8.GetBytes(str), DefaultSeed64);
+        }
+
+        /// <summary>
+        /// Computes the 64 FastHash. 
+        /// </summary>
+        /// <param name="data">Input data.</param>
+        /// <returns>64 bits Fasthash of <paramref name="data"/>.</returns>
+        public static ulong FastHash64(byte[] data)
+        {
+            return FastHash64(data, DefaultSeed64);
+        }
+
+        /// <summary>
+        /// Computes the 32 FastHash. 
+        /// </summary>
+        /// <param name="data">Input data.</param>
+        /// <param name="seed">Seed.</param>
+        /// <returns>32 bits Fasthash of <paramref name="data"/>.</returns>
+        public static uint FastHash32(byte[] data, uint seed)
+        {
+            ulong hash = FastHash64(data, seed);
+
+            // Convert the 64 bits hashcode to Fermat residue to retain information on both the higher and lower parts of the hashcode
+            return (uint) (hash - (hash >> 32));
+        }
+
+        /// <summary>
+        /// Computes the 64 FastHash. 
+        /// </summary>
+        /// <param name="data">Input data.</param>
+        /// <param name="seed">Seed.</param>
+        /// <returns>64 bits Fasthash of <paramref name="data"/>.</returns>
+        public static ulong FastHash64(byte[] data, ulong seed)
+        {
+            const ulong m = 0x880355f21e6d1965UL;
+
+            unchecked
+            {
+                ulong hash = seed ^ ((ulong)data.Length * m);
+
+                unsafe
+                {
+                    fixed (byte* byteSrc = data)
+                    {
+                        ulong* ulongSrc = (ulong*)byteSrc;
+
+                        for (int i = 0; i < data.Length / 8; i++)
+                        {
+                            hash ^= MerkleDamgardCompress(*ulongSrc);
+                            hash *= m;
+
+                            ulongSrc++;
+                        }
+                    }
+                }
+
+                // If the input buffer size is not exactly a multiple of sizeof(ulong) (aka 8 bytes), we need to hash the remaining data
+                int remainingBytes = data.Length & 7;
+                int index = data.Length - 1;
+                ulong v = 0;
+
+                for (; remainingBytes > 1; remainingBytes--)
+                {
+                    v ^= (ulong) data[index] << (8 * (remainingBytes - 1));
+                    index--;
+                }
+
+                if (remainingBytes == 1)
+                {
+                    v ^= data[index];
+
+                    hash ^= MerkleDamgardCompress(v);
+                    hash *= m;
+                }
+
+                return MerkleDamgardCompress(hash);
+            }
+        }
+
+        private static ulong MerkleDamgardCompress(ulong value)
+        {
+            unchecked
+            {
+                value ^= value >> 23;
+                value *= 0x2127599bf4325c37UL;
+                value ^= value >> 47;
+
+                return value;
+            }
+        }
+    }
+}
---
+++ test/Echarde.Indexing.Tests.Net46/FastHashTests.cs
@@ -0,0 +1,120 @@
+﻿// -----------------------------------------------------------------------------------
+// Copyright 2017, Gilles Zunino
+// -----------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Echarde.Indexing.Tests.Net46
+{
+    /// <summary>
+    /// FastHash unit tests.
+    /// </summary>
+    [TestClass]
+    public class FastHashTests
+    {
+        [TestMethod]
+        public void String_Hashing()
+        {
+            string dataToHash = "George Washington";
+
+            uint hash32 = FastHash.FastHash32(dataToHash);
+            Assert.IsTrue(hash32 == 0xd22f7524, "FastHash.FastHash32('{0}') == {1:x}", dataToHash, hash32);
+
+            ulong hash64 = FastHash.FastHash64(dataToHash);
+            Assert.IsTrue(hash64 == 0xf55ae617c78a5b3b, "FastHash.FastHash64('{0}') == {1:x}", dataToHash, hash64);
+        }
+
+        [TestMethod]
+        public void Byte_Hashing()
+        {
+            string dataToHash = "George Washington";
+            byte[] bufferToHash = Encoding.UTF8.GetBytes(dataToHash);
+
+            uint hash32 = FastHash.FastHash32(bufferToHash);
+            Assert.IsTrue(hash32 == 0xd22f7524, "FastHash.FastHash32('{0}') == {1:x}", dataToHash, hash32);
+
+            ulong hash64 = FastHash.FastHash64(bufferToHash);
+            Assert.IsTrue(hash64 == 0xf55ae617c78a5b3b, "FastHash.FastHash64('{0}') == {1:x}", dataToHash, hash64);
+        }
+
+        [TestMethod]
+        public void Byte_Hashing_Custom_Seed()
+        {
+            const uint customSeed32 = 0x123456;
+            const ulong customSeed64 = 0xf55ae617c78a5b3b;
+
+            string dataToHash = "aBcdefghiJkLMNoP_xRyv9$";
+
+            byte[] bufferToHash = Encoding.UTF8.GetBytes(dataToHash);
+
+            uint hash32 = FastHash.FastHash32(bufferToHash, customSeed32);
+            Assert.IsTrue(hash32 == 0xe9dcd4a7, "FastHash.FastHash32('{0}') == {1:x}", dataToHash, hash32);
+
+            ulong hash64 = FastHash.FastHash64(bufferToHash, customSeed64);
+            Assert.IsTrue(hash64 == 0xe8990a099b05f460, "FastHash.FastHash64('{0}') == {1:x}", dataToHash, hash64);
+        }
+
+        [TestMethod]
+        public void Byte_Ulong_Size_Matching_Hashing()
+        {
+            // Data is exactly 16 bytes, aka 2 * sizeof(ulong)
+            string dataToHash = "aBcdefghiJkLMNoP";
+            byte[] bufferToHash = Encoding.UTF8.GetBytes(dataToHash);
+
+            uint hash32 = FastHash.FastHash32(bufferToHash);
+            Assert.IsTrue(hash32 == 0xf950d073, "FastHash.FastHash32('{0}') == {1:x}", dataToHash, hash32);
+
+            ulong hash64 = FastHash.FastHash64(bufferToHash);
+            Assert.IsTrue(hash64 == 0xd6b03639d00106ac, "FastHash.FastHash64('{0}') == {1:x}", dataToHash, hash64);
+        }
+
+        [TestMethod]
+        public void Byte_Unaligned_Data_Hashing()
+        {
+            Dictionary<string, Tuple<ulong, ulong>> officialHashes = new Dictionary<string, Tuple<ulong, ulong>>()
+            {
+                // One byte over
+                { "aBcdefghiJkLMNoP_", new Tuple<ulong, ulong>(0x6461a1da, 0xebb62d4e5017cf28) },
+
+                // Two bytes over
+                { "aBcdefghiJkLMNoP_x", new Tuple<ulong, ulong>(0x515314ce, 0x3a3fc7a78b92dc75) },
+
+                // Three bytes over
+                { "aBcdefghiJkLMNoP_xR", new Tuple<ulong, ulong>(0x5190728e, 0xb633482f07c3babd) },
+
+                // Four bytes over
+                { "aBcdefghiJkLMNoP_xRy", new Tuple<ulong, ulong>(0xf701fef5, 0x51fba9f348fda8e8) },
+
+                // Five bytes over
+                { "aBcdefghiJkLMNoP_xRyv", new Tuple<ulong, ulong>(0x15c28cd5, 0xd5b19b30eb742805) },
+
+                // Six bytes over
+                { "aBcdefghiJkLMNoP_xRyv9", new Tuple<ulong, ulong>(0x376f897, 0xde4c1787e1c3101e) },
+
+                // Seve bytes over
+                { "aBcdefghiJkLMNoP_xRyv9$", new Tuple<ulong, ulong>(0x11978a69, 0x9775164aa90ca0b3) }
+            };
+
+
+            // Check all cases
+            foreach (string dataToHash in officialHashes.Keys)
+            {
+                Tuple<ulong, ulong> expectedResults = officialHashes[dataToHash];
+
+                ulong expectedResult32 = expectedResults.Item1;
+                ulong expectedResult64 = expectedResults.Item2;
+
+                byte[] bufferToHash = Encoding.UTF8.GetBytes(dataToHash);
+
+                uint hash32 = FastHash.FastHash32(bufferToHash);
+                Assert.IsTrue(hash32 == expectedResult32, "FastHash.FastHash32('{0}') == {1:x}", dataToHash, hash32);
+
+                ulong hash64 = FastHash.FastHash64(bufferToHash);
+                Assert.IsTrue(hash64 == expectedResult64, "FastHash.FastHash64('{0}') == {1:x}", dataToHash, hash64);
+            }
+        }
+    }
+}
